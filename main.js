6ddf3c86e83cd5ed5e009cd3c90410c451e963b7
David Edwards <davidwedwards.87@gmail.com>
2021-07-29 16:42:30 -0400

    linked bootstrap, changed button

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 // Grabbing HTML Elements
 const input = document.querySelector("input")
 const button = document.querySelector("button")
-const countryDiv = document.querySelector(".country-div")
+const countryDiv = document.querySelector(".country-data")
 
 // Add an event listener to our button
 button.addEventListener("click", () => {
     // Grab the value
     const country = input.value.trim()
     // Fetch our data
-    fetch(`https://restcountries.eu/rest/v2/name/${country}
-    `)
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
         .then(response => response.json())
         .then((data) => {
             const countryData = data[0]
             console.log(countryData)
-
+            // name, flag, region, capital, population
             const image = document.createElement("img")
-            image.scr = countryData.flag
+            image.src = countryData.flag
             image.classList.add("flag")
-            document.body.append(image)
+            countryDiv.append(image)
         })
 })
