b16b3c501da1541bcb03ddfeeadd7a07341f3318
David Edwards <davidwedwards.87@gmail.com>
2021-07-29 17:04:05 -0400

    added html varialbe to render different country data

--- main.js
+++ main.js
@@ -14,9 +14,15 @@ button.addEventListener("click", () => {
             const countryData = data[0]
             console.log(countryData)
             // name, flag, region, capital, population
-            const image = document.createElement("img")
-            image.src = countryData.flag
-            image.classList.add("flag")
-            countryDiv.append(image)
+
+            const html =
+            `
+            <h2>${countryData.name}</h2>
+            <img src="${countryData.flag}" alt="${countryData.name}" title="flag of ${countryData.name}" class="flag">
+            <h3>${countryData.region}</h3>
+            <h4>Capital: ${countryData.capital}</h4>
+            <h4>Population: ${countryData.population}</h4>
+            `
+            countryDiv.innerHTML = html
         })
 })
