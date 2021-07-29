71771cac88b4d96cf2ae13834fce487fabfe2a74
David Edwards <davidwedwards.87@gmail.com>
2021-07-29 16:05:18 -0400

    dynamic fetch request on button click; creating and appending image

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 // Grabbing HTML Elements
 const input = document.querySelector("input")
 const button = document.querySelector("button")
-const countryDiv = document.querySelector(".country-data")
+const countryDiv = document.querySelector(".country-div")
 
 // Add an event listener to our button
 button.addEventListener("click", () => {
+    // Grab the value
+    const country = input.value.trim()
     // Fetch our data
-    fetch(`https://restcountries.eu/rest/v2/name/canada
+    fetch(`https://restcountries.eu/rest/v2/name/${country}
     `)
         .then(response => response.json())
         .then((data) => {
-            console.log(data)
             const countryData = data[0]
             console.log(countryData)
+
+            const image = document.createElement("img")
+            image.scr = countryData.flag
+            image.classList.add("flag")
+            document.body.append(image)
         })
 })
